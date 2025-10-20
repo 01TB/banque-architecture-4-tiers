766b972449f89bc9db006552f9ceacd9da1db446
01TB <tsitohaina.berthin@gmail.com>
2025-10-20 10:29:31 +0300

    Création des entités

---
+++ compte-management/src/main/java/com/management/comptemanagement/entity/Utilisateur.java
@@ -0,0 +1,68 @@
+package com.management.comptemanagement.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "utilisateur", schema = "public", uniqueConstraints = {
+        @UniqueConstraint(name = "utilisateur_matricule_key", columnNames = {"matricule"})
+})
+public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "matricule", nullable = false, length = 50)
+    private String matricule;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private Integer role;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_direction", nullable = false)
+    private Direction idDirection;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+    public Direction getIdDirection() {
+        return idDirection;
+    }
+
+    public void setIdDirection(Direction idDirection) {
+        this.idDirection = idDirection;
+    }
+
+}
